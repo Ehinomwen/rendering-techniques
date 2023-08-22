7992ef7f399101a3a4ff5c212702f80b1459bd17
Ehinomwen <omoikeehinomwen@gmail.com>
2023-08-22 10:13:07 +0100

    working on React Lists

---
+++ techniques/src/App.jsx
@@ -0,0 +1,140 @@
+// import React from 'react';
+
+
+// function App(){
+//   return(
+//     <div>
+//       <h1>Animals:</h1>
+//       <ul>
+//         <li>Lions</li>
+//         <li>Cow</li>
+//         <li>Snake</li>
+//         <li>Lizard</li>
+//       </ul>
+//     </div>
+//   )
+// };
+// export default App
+
+// How to use react to write multiple of lists
+
+// First method
+
+
+// import React from 'react';
+
+// const animals = ["Lion", "cow", "Snake", "Lizard"];
+
+// function App(){
+//   return(
+//     <div>
+//       <h1>Animals: </h1>
+//       <ul>
+//         {animals.map((animal) =>{
+//           return <li key={animal}> {animal} </li>;
+//         }
+//         )}
+//       </ul>
+//     </div>
+//   )
+// };
+// export default App;
+
+// second method
+// import React from 'react';
+// function App(){
+//   const animals = ["Lion", "Cow", "Snake", "Lizard",];
+//   const animalList = animals.map((animal) => <li key= {animal}> {animal}</li>)
+//   return (
+//     <div>
+//       <h1>Animals: </h1>
+//       <ul>{animalList}</ul>
+//     </div>
+//   );
+// }
+// export default App;
+
+// N.B that a key is used to let react know the identity of each element in the list
+
+
+
+// next lesson
+// Rendering a list of component in JSX
+// props will be used here
+// props are arguments that are passed into components
+
+// import React  from "react";
+// function ListItem(props) {
+//   return <li>{props.animal}</li>
+
+// }
+
+// function List(props) {
+//   return(
+//     <ul>
+//       {props.animals.map((animal) =>{
+//         return <ListItem key={animal} animal={animal}/>
+
+//       })}
+//     </ul>
+//   );
+// }
+// function App() {
+//   const animals = ["Lion", "Cow", "Snake","Lizard"];
+
+//   return(
+//     <div>
+//       <h1>Animals: </h1>
+//       <List animals={animals}/>
+//     </div>
+//   );
+// }
+// export default App;
+
+// using conditional rendering ui
+
+// using the ternary operator 
+import React  from "react";
+
+function List(props) {
+  return(
+    <ul>
+      {props.animals.map((animal) =>{
+        return animal.startsWith("L") ? <li key={animal}>{animal}</li> : null;
+
+      })}
+    </ul>
+  );
+}
+
+// or this
+
+// function List(props) {
+//   const filteredAnimals = props.animals.filter(animal => animal.startsWith("L"));
+
+//   return (
+//     <ul>
+//       {filteredAnimals.map(animal => (
+//         <li key={animal}>{animal}</li>
+//       ))}
+//     </ul>
+//   );
+// }
+
+
+ function App() {
+   const animals = ["Lion", "Cow", "Snake","Lizard"];
+
+   return(
+     <div>
+       <h1>Animals: </h1>
+       <List animals={animals}/>
+     </div>
+   );
+ }
+
+export default App;
+
+
+
+
